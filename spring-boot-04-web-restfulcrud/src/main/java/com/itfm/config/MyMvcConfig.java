4d4e4a0259b642561c8d740c5ec8827cadd0376f
dellwhatislife <dellwhatislife@163.com>
2021-02-24 16:02:07 +0800

    feat：用户登陆、登陆验证

--- spring-boot-04-web-restfulcrud/src/main/java/com/itfm/config/MyMvcConfig.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/itfm/config/MyMvcConfig.java
@@ -10,10 +10,12 @@ package com.itfm.config;
  * @Version: 1.0
  */
 
+import com.itfm.component.LoginHandlerInterceptor;
 import com.itfm.component.MyLocaleResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -48,7 +50,18 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter {
 //                super.addViewControllers(registry);
                 registry.addViewController("/").setViewName("login");
                 registry.addViewController("/index.html").setViewName("login");
+                registry.addViewController("/main.html").setViewName("dashboard");
                 //shift + F6修改文件名称
+            }
+
+            //注册拦截器
+            @Override
+            public void addInterceptors(InterceptorRegistry registry) {
+                //super.addInterceptors(registry);
+                //静态资源；  *.css , *.js
+                //SpringBoot已经做好了静态资源映射
+                registry.addInterceptor(new LoginHandlerInterceptor()).addPathPatterns("/**")
+                        .excludePathPatterns("/index.html","/","/user/login");
             }
         };
         return adapter;
